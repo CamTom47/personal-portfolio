05dc660bb198feedd4a8fc75a91dfa6659cdc7f4
Cameron Thomas <CamThomas47@gmail.com>
2025-11-10 12:25:24 -0500

    server configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // CRA's default build output
-  },
-  server: {
-    port: 3000,
-    host: '0.0.0.0'
-  }
-})
+	plugins: [react()],
+	build: {
+		outDir: "build", // CRA's default build output
+	},
+	server: {
+		port: 3000,
+		host: true,
+	},
+});
