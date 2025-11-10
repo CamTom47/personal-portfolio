0fbf04a5f74ebf69bd80791b669b020d1fcc5d46
Cameron Thomas <CamThomas47@gmail.com>
2025-11-10 13:31:10 -0500

    updating pdf paths

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "vite-react-app",
-    script: "serve - build", // or "server.js" if using Express
+    script: "serve build", // or "server.js" if using Express
     args: "-s build -l 8080", // arguments for 'serve'
     env: {
       NODE_ENV: "production",
